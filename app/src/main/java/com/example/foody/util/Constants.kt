d3f1613cacfed52a87d84591569211a0221cbb78
rohit7872 <rohit.speroinfosystems@gmail.com>
2024-08-19 15:02:48 +0530

    Added new api key

--- app/src/main/java/com/example/foody/util/Constants.kt
+++ app/src/main/java/com/example/foody/util/Constants.kt
@@ -5,7 +5,7 @@ class Constants {
     companion object {
 
         const val BASE_URL = "https://api.spoonacular.com"
-        const val API_KEY = "[YOUR API KEY HERE]"
+        const val API_KEY = "d30d45031fe1421da0f8cfffeb46b0bb"
 
         // API Query Keys
         const val QUERY_SEARCH = "query"
